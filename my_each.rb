c38b63b0c041aef05fd4d2e843f266506a3890e1
Maurice Falls <falls8019oct@gmail.com>
2020-03-15 23:40:19 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,21 +1,10 @@
-def hello_t(array)
-  if block_given?
-    i = 0
-    
-    while i < array.length
-      yield array[i]
-      i = i + 1
-    end
-    array
-  else
-    puts "Hey! No block was given!"
+def my_each(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
   end
+  array
 end
 
-# call your method here!
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
